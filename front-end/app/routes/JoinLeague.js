4cbdee73805bcfa08dae24eb4e1b67b59e35678d
GWilman <georgewilman1@gmail.com>
2018-02-18 16:28:34 +0000

    Front end code validation for joining league

--- front-end/app/routes/JoinLeague.js
+++ front-end/app/routes/JoinLeague.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { Actions } from 'react-native-router-flux';
+// import { Actions } from 'react-native-router-flux';
 import styles from './styles';
 
 // import Auth from '../lib/Auth';
@@ -10,7 +10,8 @@ class JoinLeague extends Component {
   constructor() {
     super();
     this.state = {
-      leagues: null
+      leagues: null,
+      code: null
     };
   }
 
@@ -24,36 +25,54 @@ class JoinLeague extends Component {
       .done();
   }
 
-  // handleChange = ({ target: { name, value } }) => {
-  //   const league = Object.assign({}, this.state.league, { [name]: value });
-  //   this.setState({ league });
-  // }
-  //
-  // handleSubmit() {
-  //   fetch('http://127.0.0.1:3001/leagues', {
-  //     method: 'POST',
-  //     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({
-  //       name: this.state.name,
-  //       stake: this.state.stake
-  //     })
-  //   })
-  //     .then(league => console.log(league))
-  //     .then(() => {
-  //       Alert.alert('League created!');
-  //       Actions.Dashboard();
-  //     })
-  //     .done();
-  // }
+  handleSubmit(league) {
+    const code = this.state.leagues.find(_league => _league.id === league.id).code;
+    console.log('code', code);
+    console.log('input', this.state.code);
+    if (code !== this.state.code) {
+      return Alert.alert('Invalid code');
+    }
+    Alert.alert('correct code!');
+    console.log('correct code!');
+
+    // fetch('http://127.0.0.1:3001/leagues', {
+    //   method: 'POST',
+    //   headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({
+    //     name: this.state.name,
+    //     stake: this.state.stake
+    //   })
+    // })
+    //   .then(league => console.log(league))
+    //   .then(() => {
+    //     Alert.alert('League created!');
+    //     Actions.Dashboard();
+    //   })
+    //   .done();
+  }
 
   render() {
-    console.log('leagues', this.state.leagues);
     return (
       <View style={styles.container}>
+        {this.state.code && <Text>{this.state.code}</Text>}
         <Text style={styles.title}>Join a League</Text>
         { this.state.leagues && this.state.leagues.map(league =>
-          <View key={league._id}>
+          <View key={league._id} style={styles.leagueJoinContainer}>
             <Text>{league.name}</Text>
+            <Text>{league.stake}</Text>
+            <View style={styles.form}>
+              <TextInput
+                editable={true}
+                onChangeText={(code) => this.setState({ code: parseInt(code, 10) })}
+                placeholder='Enter code'
+                returnKeyType='next'
+                style={styles.inputText}
+                value={league.entryCode}
+              />
+              <TouchableOpacity style={styles.buttonWrapper} onPress={() => this.handleSubmit(league)}>
+                <Text style={styles.buttonText}>Join</Text>
+              </TouchableOpacity>
+            </View>
           </View>)
         }
       </View>
--- front-end/app/routes/styles.js
+++ front-end/app/routes/styles.js
@@ -1,36 +1,55 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+
   buttonText: {
     fontSize: 20,
     padding: 10,
     textAlign: 'center'
   },
+
   buttonWrapper: {
     backgroundColor: '#D3D3D3',
     marginBottom: 10,
     width: 300
   },
+
   container: {
     alignItems: 'center',
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'space-around'
   },
+
+  leagueJoinContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#e8e8e8',
+    alignItems: 'center',
+    flex: 1,
+    width: '90%',
+    justifyContent: 'center',
+    margin: 30
+  },
+
   form: {
     width: 300
   },
+
   image: {
     margin: 10
   },
+
   inputText: {
     marginBottom: 10,
     padding: 10
   },
+
   title: {
     fontSize: 40,
     margin: 10,
     textAlign: 'center'
   }
+
 });
 
 export default styles;
