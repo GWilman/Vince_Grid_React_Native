4f6598403e0f6ed98f61771447c6226856477554
GWilman <georgewilman1@gmail.com>
2018-02-20 21:46:21 +0000

    removing unused code

--- server/reference/quoter.js
+++
@@ -1,7 +0,0 @@
-const quotes = require('./quotes.json');
-
-exports.getRandomOne = function() {
-  const totalAmount = quotes.length;
-  const rand = Math.floor(Math.random() * totalAmount);
-  return quotes[rand];
-};
